e5c2f2859b4a7ff2548e105622924402eb1dfe10
Tim Neumegen <tneumegen@gmail.com>
2025-10-26 10:19:13 +1300

    Moved timing

--- Main.gd
+++ Main.gd
@@ -4,12 +4,9 @@ const MAX_INT: int = 100
 const ARRAY_SIZE = 1000000
 
 func _ready() -> void:
-	var first = _create_array(ARRAY_SIZE)
-	var check = _sum(first)
-	var before = Time.get_ticks_msec()
-	var result = _gdscript_prefix_sum(first)
-	var time = Time.get_ticks_msec() - before
-	prints ("GDScript", time, result[result.size() -1] == check)
+	var original = _create_array(ARRAY_SIZE)
+	var check = _sum(original)
+	_timed_run("GDScript Linear", original.duplicate(), check, _gdscript_prefix_sum)
 
 func _sum(numbers: Array[int]) -> int:
 	var total: int = 0
@@ -24,6 +21,12 @@ func _create_array(size: int) -> Array[int]:
 	for i in size:
 		numbers[i] = randi_range(MIN_INT, MAX_INT)
 	return numbers
+
+func _timed_run(functionName: String, numbers: Array[int], check: int, function: Callable) -> void:
+	var before = Time.get_ticks_msec()
+	var result = function.call(numbers)
+	var time = Time.get_ticks_msec() - before
+	prints (functionName, time, "milliseconds, valid:", result[result.size() -1] == check)
 
 func _gdscript_prefix_sum(numbers: Array[int]) -> Array[int]:
 	for i in range(1, numbers.size()):
