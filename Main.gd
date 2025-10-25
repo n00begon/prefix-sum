9f1a3a89d6051e7c0b8d356cda0e969b58f327db
Tim Neumegen <tneumegen@gmail.com>
2025-10-25 16:53:20 +1300

    gdscript version

--- Main.gd
+++ Main.gd
@@ -1,15 +1,29 @@
 extends Node2D
-const MIN_INT: int = 0
-const MAX_INT: int = 100
+const MIN_INT: int = 1
+const MAX_INT: int = 1
 
 func _ready() -> void:
 	var first = _create_array(50)
-	prints ("Numbers", first)
-	
+	var check = _sum(first)
+	prints(check, first)
+	var result = _gdscript_prefix_sum(first)
+	prints ("Numbers", result[result.size() -1] == check, result)
+
+func _sum(numbers: Array[int]) -> int:
+	var total: int = 0
+	for i in numbers:
+		total += i
+	return total
+
 func _create_array(size: int) -> Array[int]:
 	var numbers: Array[int] = []
 	numbers.resize(size)
 	
 	for i in size:
 		numbers[i] = randi_range(MIN_INT, MAX_INT)
+	return numbers
+
+func _gdscript_prefix_sum(numbers: Array[int]) -> Array[int]:
+	for i in range(1, numbers.size()):
+		numbers[i] += numbers[i - 1]
 	return numbers
