d213b0c34ecacba756235e1880705d0e6a8747a0
Tim Neumegen <tneumegen@gmail.com>
2025-11-16 23:06:28 +1300

    Added Blelloch Algorithm
    Spelt Steele correctly
    Improved the checking to validate every number
    Added two single threaded implementations of Blelloch, one close to the Nvidia examples and an iteration that helped me multithread it
    Added a multithreaded implementation of Blelloch

--- Main.gd
+++ Main.gd
@@ -6,18 +6,29 @@ const RANDOM: bool = false
 
 func _ready() -> void:
 	var original = create_array(SIZE)
-	var check = sum(original)
+	var check = create_check_array(original.duplicate_deep())
 	timed_run("GDScript Linear", original.duplicate_deep(), check, gdscript_linear)
-	timed_run("GDScript Hillis Steele Singlethread", original.duplicate_deep(), check, gdscript_hillissteel_single_thread)
-	timed_run("GDScript Hillis Steele Multithreaded", original.duplicate_deep(), check, gdscript_hillissteel_multi_threaded)
+	timed_run("GDScript Hillis Steele Singlethread", original.duplicate_deep(), check, gdscript_hillissteele_single_thread)
+	timed_run("GDScript Hillis Steele Multithreaded", original.duplicate_deep(), check, gdscript_hillissteele_multi_threaded)
+	timed_run("GDScript Blelloch Singlethread Gem", original.duplicate_deep(), check, gdscript_blelloch_singlethread_gem)
+	timed_run("GDScript Blelloch Singlethread Named", original.duplicate_deep(), check, gdscript_blelloch_singlethread_named)
+	timed_run("GDScript Blelloch Multithreaded", original.duplicate_deep(), check, gdscript_blelloch_multithreaded)
 
-func sum(numbers: Array[int]) -> int:
+func create_check_array(numbers: Array[int]) -> Array[int]:
 	var total: int = 0
 	for i in numbers:
 		total += i
-	return total
+
+	var result: Array[int] = gdscript_linear(numbers)
+
+	assert(result[result.size() - 1] == total)
+	return result
 
 func create_array(size: int) -> Array[int]:
+	var label: Label = Label.new()
+	label.text = "Size: " + str(size)
+	%Results.add_child(label)
+	
 	var numbers: Array[int] = []
 	numbers.resize(size)
 	if RANDOM:
@@ -28,21 +39,36 @@ func create_array(size: int) -> Array[int]:
 			numbers[i] = i + 1
 	return numbers
 
-func timed_run(functionName: String, numbers: Array[int], check: int, function: Callable) -> void:
-	var before = Time.get_ticks_msec()
-	var result = function.call(numbers)
-	var time = Time.get_ticks_msec() - before
-	var total = result[result.size() -1]
-	prints (functionName, time, "milliseconds, valid:", total == check)
-	if !total == check:
-		prints("Expected", check, "Actual", total)
+func timed_run(functionName: String, numbers: Array[int], check: Array[int], function: Callable) -> void:
+	var before: int = Time.get_ticks_msec()
+	var result: Array[int] = function.call(numbers)
+	var time: int = Time.get_ticks_msec() - before
+	var label: Label = Label.new()
+	var output: String = str(functionName) + " " + str(time) + " milliseconds"
+	if check.size() != result.size():
+		output += ", Invalid! expected length " + str(check.size()) + ", actual " + str(result.size()) 
+	
+	var differences: int = 0
+	
+	for i in range(check.size()):
+		if check[i] != result[i]:
+			differences += 1
+	
+	if differences > 0:
+		output += ", Invalid! Found " + str(differences) + " differences from the check array"
+	
+	print(output)
+	label.text = output
+	%Results.add_child(label)
 
+# The single threaded approach
 func gdscript_linear(numbers: Array[int]) -> Array[int]:
 	for i in range(1, numbers.size()):
 		numbers[i] += numbers[i - 1]
 	return numbers
 
-func gdscript_hillissteel_single_thread(numbers: Array[int]) -> Array[int]:
+#  Hillis Steele in a single thread to learn the algorithm and comparison
+func gdscript_hillissteele_single_thread(numbers: Array[int]) -> Array[int]:
 	var size: int = numbers.size()
 	var buffer: Array[int] = numbers.duplicate_deep()
 	var old = numbers
@@ -58,8 +84,9 @@ func gdscript_hillissteel_single_thread(numbers: Array[int]) -> Array[int]:
 		old = new
 		new = temp
 	return old
-
-func gdscript_hillissteel_multi_threaded(numbers: Array[int]) -> Array[int]:
+	
+#  Hillis Steele in a multithread in gdscript to learn the algorithm and comparison
+func gdscript_hillissteele_multi_threaded(numbers: Array[int]) -> Array[int]:
 	var size: int = numbers.size()
 	var buffer: Array[int] = numbers.duplicate_deep()
 	var old = numbers
@@ -69,7 +96,7 @@ func gdscript_hillissteel_multi_threaded(numbers: Array[int]) -> Array[int]:
 	var elements = ceil(size/float(blockSize))
 
 	for step in range(0, ceil(log(size) / log(2))):
-		var callable = Callable(self, "_hillissteel_neighbours").bind(blockSize, step, old, new)
+		var callable = Callable(self, "_hillissteele_neighbours").bind(blockSize, step, old, new)
 		var task_id = WorkerThreadPool.add_group_task(callable, elements)
 		WorkerThreadPool.wait_for_group_task_completion(task_id)
 		var temp = old
@@ -77,7 +104,8 @@ func gdscript_hillissteel_multi_threaded(numbers: Array[int]) -> Array[int]:
 		new = temp
 	return old
 
-func _hillissteel_neighbours(block:int, blockSize: int, step: int, old: Array[int], new: Array[int]) -> void:
+# The thread specific part of Hillis Steele
+func _hillissteele_neighbours(block:int, blockSize: int, step: int, old: Array[int], new: Array[int]) -> void:
 	var start: int = block * blockSize
 	var end: int = min(start + blockSize, old.size())
 	for i: int in range(start, end):
@@ -85,3 +113,112 @@ func _hillissteel_neighbours(block:int, blockSize: int, step: int, old: Array[in
 			new[i] = old[i - 2 ** step] + old[i]
 		else:
 			new[i] = old[i]
+
+# Blelloch algorithm following https://developer.nvidia.com/gpugems/gpugems3/part-vi-gpu-computing/chapter-39-parallel-prefix-sum-scan-cuda
+func gdscript_blelloch_singlethread_gem(numbers: Array[int]) -> Array[int]:
+	var originalSize: int = numbers.size()
+	
+	var maxSteps: int = ceil(log(originalSize) / log(2))
+	var paddedSize: int = 2 ** maxSteps
+	numbers.resize(paddedSize + 1) # Have an extra spot to turn it into an inclusive sum
+
+	# Upsweep
+	for step in range(0, maxSteps):
+		var stepSize = 2 ** step
+		for i in range(stepSize - 1, paddedSize, stepSize * 2):
+			numbers[i + stepSize] = numbers[i] + numbers[i + stepSize]
+	# Store the end result in the spare slot
+	numbers[paddedSize] =  numbers[paddedSize - 1]
+	
+	# Downsweep
+	numbers[paddedSize - 1] = 0
+
+	for step in range(maxSteps - 1, -1, -1):
+		var stepSize = 2 ** step
+		for i in range(stepSize - 1, paddedSize, stepSize * 2):
+			var temp = numbers[i + stepSize]
+			numbers[i + stepSize] = numbers[i] + numbers[i + stepSize]
+			numbers[i] = temp
+	
+	# Trim to change it to inclusive
+	return numbers.slice(1, originalSize + 1)
+
+# Blelloch Algoirthm with the parts separated out to make it easier to multithread
+func gdscript_blelloch_singlethread_named(numbers: Array[int]) -> Array[int]:
+	var originalSize: int = numbers.size()
+	var maxSteps: int = ceil(log(originalSize) / log(2))
+	var paddedSize: int = 2 ** maxSteps
+	numbers.resize(paddedSize + 1) # Have an extra spot to turn it into an inclusive sum
+
+	# Upsweep
+	for step in range(0, maxSteps):
+		var stepSize: int = 2 ** step
+		var subSteps: int = ceil(float(paddedSize)/stepSize) / 2
+		for i in range(subSteps):
+			var first: int = i * stepSize * 2 + stepSize - 1 
+			var second: int = first + stepSize
+			numbers[second] = numbers[first] + numbers[second]
+
+	# Store the end result in the spare slot
+	numbers[paddedSize] =  numbers[paddedSize - 1]
+
+	# Downsweep
+	numbers[paddedSize - 1] = 0
+
+	for step in range(maxSteps - 1, -1, -1):
+		var stepSize = 2 ** step
+		var subSteps: int = ceil(float(paddedSize)/stepSize) / 2
+		for i in range(subSteps):
+			var first: int = i * stepSize * 2 + stepSize - 1 
+			var second: int = first + stepSize
+			var temp = numbers[second]
+			numbers[second] = numbers[first] + numbers[second]
+			numbers[first] = temp
+	
+	# Trim to change it to inclusive
+	return numbers.slice(1, originalSize + 1)
+
+# Blelloch Algoirthm multithread
+func gdscript_blelloch_multithreaded(numbers: Array[int]) -> Array[int]:
+	var originalSize: int = numbers.size()
+	var maxSteps: int = ceil(log(originalSize) / log(2))
+	var paddedSize: int = 2 ** maxSteps
+	numbers.resize(paddedSize + 1) # Have an extra spot to turn it into an inclusive sum
+
+	# Upsweep
+	for step in range(0, maxSteps):
+		var stepSize: int = 2 ** step
+		var elements: int = ceil(float(paddedSize)/stepSize) / 2
+		var callable = Callable(self, "_upsweep").bind(stepSize, numbers)
+		var taskId = WorkerThreadPool.add_group_task(callable, elements)
+		WorkerThreadPool.wait_for_group_task_completion(taskId)
+
+	# Store the end result in the spare slot
+	numbers[paddedSize] =  numbers[paddedSize - 1]
+
+	# Downsweep
+	numbers[paddedSize - 1] = 0
+	
+	for step in range(maxSteps - 1, -1, -1):
+		var stepSize = 2 ** step
+		var elements: int = ceil(float(paddedSize)/stepSize) / 2
+		var callable = Callable(self, "_downsweep").bind(stepSize, numbers)
+		var taskId = WorkerThreadPool.add_group_task(callable, elements)
+		WorkerThreadPool.wait_for_group_task_completion(taskId)
+		
+	# Trim to change it to inclusive
+	return numbers.slice(1, originalSize + 1)
+
+# Blelloch Algoirthm upsweep individual thread code
+func _upsweep(index: int, stepSize: int, numbers: Array[int]) -> void:
+	var first: int = index * stepSize * 2 + stepSize - 1 
+	var second: int = first + stepSize
+	numbers[second] = numbers[first] + numbers[second]
+
+# Blelloch Algoirthm downsweep individual thread code
+func _downsweep(index: int, stepSize: int, numbers: Array[int]) -> void:
+	var first: int = index * stepSize * 2 + stepSize - 1 
+	var second: int = first + stepSize
+	var temp = numbers[second]
+	numbers[second] = numbers[first] + numbers[second]
+	numbers[first] = temp
--- README.md
+++ README.md
@@ -6,5 +6,11 @@ There are two algorithms which allow Parallel Prefix Sums, Hillis Steele and Ble
 
 ### References
 * [Hillis Steele Original Paper](https://dl.acm.org/doi/pdf/10.1145/7902.7903)
-* [Geeks for Geeks Explaination](https://www.geeksforgeeks.org/cpp/hillis-steele-scan-parallel-prefix-scan-algorithm/)
+* [Blelloch Original Paper](https://people.eecs.berkeley.edu/~culler/cs262b/papers/scan89.pdf)
+* [Geeks for Geeks Explaination of Hillis Steele](https://www.geeksforgeeks.org/cpp/hillis-steele-scan-parallel-prefix-scan-algorithm/)
 * [NVidia GPU Gems](https://developer.nvidia.com/gpugems/gpugems3/part-vi-gpu-computing/chapter-39-parallel-prefix-sum-scan-cuda)
+
+### Videos
+* [Hillis Steele](9https://www.youtube.com/watch?v=RdfmxfZBHpo)
+* [Blelloch](https://www.youtube.com/watch?v=mmYv3Haj6uc)
+* [Prefix Sum Lecture](https://www.youtube.com/watch?v=lavZl_wEbPE)
